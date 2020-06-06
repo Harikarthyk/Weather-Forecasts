f14824d320054cb0e2f76cc4d37290290bf5756e
Hari_karthyk <54505967+Harikarthyk@users.noreply.github.com>
2020-06-07 00:23:15 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		if( event.keyCode===13 ){
 			var city = $('.inputValue').val();
 			// console.log(city) ;
-			$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=131cd68909b623f5d5cfb417e05ee351",function(data){
+			$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid={YOUR_APPID}",function(data){
 				console.log(data) ;
 				var name = data.name+","+data.sys.country ;
 				var degree = Math.round(data.main.temp); 
