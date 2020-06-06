c817b63250638a39d5a7dc9626bad046055eba68
Hari_karthyk <54505967+Harikarthyk@users.noreply.github.com>
2020-06-07 00:14:41 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	// alert("h") ;
+	$('.inputValue').on('keyup',function(event){
+		if( event.keyCode===13 ){
+			var city = $('.inputValue').val();
+			// console.log(city) ;
+			$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=131cd68909b623f5d5cfb417e05ee351",function(data){
+				console.log(data) ;
+				var name = data.name+","+data.sys.country ;
+				var degree = Math.round(data.main.temp); 
+				var description = data.weather[0].description ;
+				var icon = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png" ;
+				$('.city').text(name) ;
+				$('.degree').text(degree+"'C") ;
+				$('.icon').attr("src",icon) ;
+				$('.description').text(description) ;
+				$('.metric').show();
+			
+			});
+			$(this).val('') ;
+			// city = '' ;
+		}
+	});
+});
